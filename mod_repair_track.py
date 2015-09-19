7b62ca700949fafad883bf0b1fd679c89909adf5
ushmal <ushmal@gmail.com>
2015-09-19 16:33:16 +0300

    fix dev hook and glob

--- mod_repair_track.py
+++ mod_repair_track.py
@@ -5,34 +5,32 @@ from Avatar import PlayerAvatar
 from gui.Scaleform.Battle import DamagePanel
 from gui.battle_control import g_sessionProvider
 from debug_utils import *
-global devName
-global devState
-global _
+devName = None
+devState = None
 
 
-def new_updateDeviceState(self, value):
+def new_as_updateDeviceStateS(self, deviceName, deviceState):
     global devName
     global devState
-    global _
-    old_updateDeviceState(self, value)
-    devName, devState, _ = value
+    old_as_updateDeviceStateS(self, deviceName, deviceState)
+    devName = deviceName
+    devState = deviceState
 
 
 def new_handleKey(self, isDown, key, mods):
     global devName
     global devState
-    global _
-    old_handleKey(self, isDown, key, mods)
     player = BigWorld.player()
     if player and player.isOnArena:
         if player.inputHandler and key == Keys.KEY_SPACE:
             if devName in ('chassis', 'rightTrack', 'leftTrack') and devState == 'destroyed':
                 BigWorld.callback(0.09, partial(g_sessionProvider.getEquipmentsCtrl().changeSettingByTag, 'repairkit', devName, BigWorld.player()))
                 LOG_NOTE('track repaired')
+    return old_handleKey(self, isDown, key, mods)
 
 
-old_updateDeviceState = DamagePanel._updateDeviceState
-DamagePanel._updateDeviceState = new_updateDeviceState
+old_as_updateDeviceStateS = DamagePanel.as_updateDeviceStateS
+DamagePanel.as_updateDeviceStateS = new_as_updateDeviceStateS
 old_handleKey = PlayerAvatar.handleKey
 PlayerAvatar.handleKey = new_handleKey
 
