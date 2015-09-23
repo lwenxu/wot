6206dc1372335e1d2f0c4ec0567c4a4d3e8a5434
ushmal <ushmal@gmail.com>
2015-09-23 18:01:47 +0300

    wait for loading

--- mod_auto_equip.py
+++ mod_auto_equip.py
@@ -2,16 +2,16 @@ import BigWorld
 import ResMgr
 from CurrentVehicle import g_currentVehicle
 from gui.Scaleform.daapi.view.lobby.hangar.AmmunitionPanel import AmmunitionPanel
-from gui.ClientHangarSpace import ClientHangarSpace
+from gui.ClientHangarSpace import ClientHangarSpace, _VehicleAppearance
 from gui.shared import g_itemsCache, REQ_CRITERIA
 from debug_utils import *
 from gui import SystemMessages
 g_xmlSetting = None
 g_prevVehicleID = None
 g_started = False
+g_vAppearance = None
 
-
-def equipAllRemovableDevicesOnVehicle(vehicle):
+def equipOptionalDevicesOnVehicle(vehicle):
     global g_xmlSetting
     try:
         if g_xmlSetting[vehicle.name]:
@@ -21,49 +21,59 @@ def equipAllRemovableDevicesOnVehicle(vehicle):
                     deviceCompactDescr = g_xmlSetting[vehicle.name].readInt('slot' + str(slotIdx + 1), 0)
                     if deviceCompactDescr is not 0:
                         BigWorld.player().inventory.equipOptionalDevice(vehicle.invID, deviceCompactDescr, slotIdx, False, None)
-                        LOG_NOTE('equip', vehicle.name, slotIdx, deviceCompactDescr)
+                        #LOG_NOTE('equip', vehicle.name, slotIdx, deviceCompactDescr)
     except:
         LOG_CURRENT_EXCEPTION()
 
 
-def removeAllRemovableDevicesFromVehicle(vehicle):
+def removeAllOptionalDevicesFromVehicle(vehicle):
     try:
         if vehicle and not (vehicle.isInBattle or vehicle.isLocked):
             for slotIdx in range(0, 3):
                 device = vehicle.descriptor.optionalDevices[slotIdx]
                 if device and device.removable:
                     BigWorld.player().inventory.equipOptionalDevice(vehicle.invID, 0, slotIdx, False, None)
-                    LOG_NOTE('remove:', vehicle.name, slotIdx, device.name)
+                    #LOG_NOTE('remove:', vehicle.name, slotIdx, device.name)
     except:
         LOG_CURRENT_EXCEPTION()
 
 
-def removeAllRemovableDevicesFromAllVehicles(curVehicle):
+def removeAllOptionalDevices(curVehicle):
     vehicles = g_itemsCache.items.getVehicles(REQ_CRITERIA.INVENTORY).values()
     for vehicle in vehicles:
         if curVehicle is not vehicle:
-            removeAllRemovableDevicesFromVehicle(vehicle)
+            removeAllOptionalDevicesFromVehicle(vehicle)
 
 
 def equipCurrentVehicle():
-    if g_currentVehicle.isInHangar:
-        curVehicle = g_currentVehicle.item
-        LOG_NOTE('try to equip: %s' % curVehicle.name)
-        removeAllRemovableDevicesFromAllVehicles(curVehicle)
-        equipAllRemovableDevicesOnVehicle(curVehicle)
+    global g_vAppearance
+    if g_vAppearance is not None:
+        if g_vAppearance._VehicleAppearance__isLoaded:
+            if g_currentVehicle.isInHangar:
+                curVehicle = g_currentVehicle.item
+                #LOG_NOTE('try to equip: %s' % curVehicle.name)
+                removeAllOptionalDevices(curVehicle)
+                equipOptionalDevicesOnVehicle(curVehicle)
+        else:
+            BigWorld.callback(0.2, equipCurrentVehicle)
 
 
 def new_recreateVehicle(self, vDesc, vState, onVehicleLoadedCallback):
     global old_recreateVehicle
     old_recreateVehicle(self, vDesc, vState, onVehicleLoadedCallback)
-    global g_prevVehicleID
-    if g_prevVehicleID != g_currentVehicle.item.intCD:
-        g_prevVehicleID = g_currentVehicle.item.intCD
-        BigWorld.callback(0.5, equipCurrentVehicle)
+    try:
+        global g_vAppearance
+        g_vAppearance = self._ClientHangarSpace__vAppearance
+        global g_prevVehicleID
+        if g_prevVehicleID != g_currentVehicle.item.intCD:
+            g_prevVehicleID = g_currentVehicle.item.intCD
+            equipCurrentVehicle()
+    except:
+        LOG_CURRENT_EXCEPTION()
 
 
 def saveDeviceOnVehicle(vehicle, deviceId, slotId, isRemove):
-    LOG_NOTE('save', vehicle.name, slotId, deviceId)
+    #LOG_NOTE('save', vehicle.name, slotId, deviceId)
     g_xmlSetting.write(vehicle.name, '')
     if isRemove:
         g_xmlSetting[vehicle.name].writeInt('slot' + str(slotId + 1), 0)
