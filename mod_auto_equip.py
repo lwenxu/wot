a50477624a49928c8e070e09b94cbfe5d62c3edb
ushmal <ushmal@gmail.com>
2015-10-07 16:17:24 +0300

    add crew return

--- mod_auto_equip.py
+++ mod_auto_equip.py
@@ -10,19 +10,21 @@ g_xmlSetting = None
 g_prevVehicleID = None
 g_started = False
 g_vAppearance = None
+g_autoEquip = True
+g_returnCrew = True
 
 
-def equipOptionalDevicesOnVehicle(vehicle):
+def equipOptionalDevices(curVehicle):
     global g_xmlSetting
     try:
-        if g_xmlSetting[vehicle.name]:
+        if g_xmlSetting[curVehicle.name]:
             for slotIdx in range(0, 3):
-                device = vehicle.descriptor.optionalDevices[slotIdx]
+                device = curVehicle.descriptor.optionalDevices[slotIdx]
                 if not device:
-                    deviceCompactDescr = g_xmlSetting[vehicle.name].readInt('slot' + str(slotIdx + 1), 0)
+                    deviceCompactDescr = g_xmlSetting[curVehicle.name].readInt('slot' + str(slotIdx + 1), 0)
                     if deviceCompactDescr is not 0:
-                        BigWorld.player().inventory.equipOptionalDevice(vehicle.invID, deviceCompactDescr, slotIdx, False, None)
-                        #LOG_NOTE('equip', vehicle.name, slotIdx, deviceCompactDescr)
+                        BigWorld.player().inventory.equipOptionalDevice(curVehicle.invID, deviceCompactDescr, slotIdx, False, None)
+                        LOG_NOTE('equip', curVehicle.name, slotIdx, deviceCompactDescr)
     except:
         LOG_CURRENT_EXCEPTION()
 
@@ -34,7 +36,7 @@ def removeAllOptionalDevicesFromVehicle(vehicle):
                 device = vehicle.descriptor.optionalDevices[slotIdx]
                 if device and device.removable:
                     BigWorld.player().inventory.equipOptionalDevice(vehicle.invID, 0, slotIdx, False, None)
-                    #LOG_NOTE('remove:', vehicle.name, slotIdx, device.name)
+                    LOG_NOTE('remove:', vehicle.name, slotIdx, device.name)
     except:
         LOG_CURRENT_EXCEPTION()
 
@@ -42,19 +44,31 @@ def removeAllOptionalDevicesFromVehicle(vehicle):
 def removeAllOptionalDevices(curVehicle):
     vehicles = g_itemsCache.items.getVehicles(REQ_CRITERIA.INVENTORY).values()
     for vehicle in vehicles:
-        if curVehicle is not vehicle:
+        if curVehicle.intCD != vehicle.intCD:
             removeAllOptionalDevicesFromVehicle(vehicle)
 
 
+def returnCrew(curVehicle):
+    global g_returnCrew
+    if not curVehicle.isCrewFull:
+        BigWorld.player().inventory.returnCrew(curVehicle.invID, None)
+        LOG_NOTE('return crew: %s' % curVehicle.name)
+
+
 def equipCurrentVehicle():
+    global g_autoEquip
+    global g_returnCrew
     global g_vAppearance
     if g_vAppearance is not None:
         if g_vAppearance._VehicleAppearance__isLoaded:
-            if g_currentVehicle.isInHangar:
+            if g_currentVehicle.isInHangar():
                 curVehicle = g_currentVehicle.item
-                #LOG_NOTE('try to equip: %s' % curVehicle.name)
-                removeAllOptionalDevices(curVehicle)
-                equipOptionalDevicesOnVehicle(curVehicle)
+                LOG_NOTE('try to equip: %s' % curVehicle.name)
+                if g_autoEquip:
+                    removeAllOptionalDevices(curVehicle)
+                    equipOptionalDevices(curVehicle)
+                if g_returnCrew:
+                    returnCrew(curVehicle)
         else:
             BigWorld.callback(0.2, equipCurrentVehicle)
 
