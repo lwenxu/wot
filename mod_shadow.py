9fcbdb5c56ef0517712cb70681dfdbb78f138412
ushmal <ushmal@gmail.com>
2015-10-14 15:38:23 +0300

    fix dead shadows

--- mod_shadow.py
+++ mod_shadow.py
@@ -9,34 +9,30 @@ from VehicleAppearance import VehicleAppearance, StippleManager, _VEHICLE_DISAPP
 from gui.app_loader import g_appLoader
 from debug_utils import *
 from functools import partial
-g_key = Keys.KEY_NUMPAD5
-g_disappearDelay = 20
-g_disappearDelayEnabled = True
-g_disappearCaption = True
-g_times = []
 
+g_active = True
+g_key = Keys.KEY_NUMPAD5
+g_delay = 20
 g_xmlConfig = ResMgr.openSection('scripts/client/gui/mods/mod_shadow.xml')
 if g_xmlConfig:
-    g_disappearDelay = g_xmlConfig.readInt('delay', g_disappearDelay)
-    if g_disappearDelay > 0:
-        g_disappearDelayEnabled = g_xmlConfig.readBool('active', True)
+    g_delay = g_xmlConfig.readInt('delay', g_delay)
+    if g_delay > 0:
+        g_active = g_xmlConfig.readBool('active', True)
     else:
-        g_disappearDelayEnabled = False
+        g_active = False
     g_key = getattr(Keys, g_xmlConfig.readString('key', 'KEY_NUMPAD5'))
-    g_disappearCaption = g_xmlConfig.readBool('caption', True)
-    LOG_NOTE('config is loaded')
+    LOG_DEBUG('config is loaded')
 
 def new_handleKey(self, isDown, key, mods):
-    global g_disappearDelayEnabled
-    global g_key
+    global g_active
     if key == g_key and mods == 0 and isDown:
         if g_appLoader.getDefBattleApp() is not None:
-            if g_disappearDelayEnabled:
+            if g_active:
                 g_appLoader.getDefBattleApp().call('battle.PlayerMessagesPanel.ShowMessage', ['0', 'Shadow OFF', 'red'])
-                g_disappearDelayEnabled = False
+                g_active = False
             else:
                 g_appLoader.getDefBattleApp().call('battle.PlayerMessagesPanel.ShowMessage', ['0', 'Shadow ON', 'gold'])
-                g_disappearDelayEnabled = True
+                g_active = True
             self.soundNotifications.play('chat_shortcut_common_fx')
             return True
     return old_handleKey(self, isDown, key, mods)
@@ -44,52 +40,77 @@ def new_handleKey(self, isDown, key, mods):
 old_handleKey = PlayerAvatar.handleKey
 PlayerAvatar.handleKey = new_handleKey
 
+g_shadows_list = []
+
 def new_addStippleModel(self, vehID):
-    global g_times
-    global g_disappearDelay
-    global g_disappearDelayEnabled
-    global g_disappearCaption
-    stippleModel = self._StippleManager__stippleToAddDescs[vehID][0]
-    if stippleModel.attached:
-        CallAddStippleMode = BigWorld.callback(0.0, partial(self._StippleManager__addStippleModel, vehID))
-        self._StippleManager__stippleToAddDescs[vehID] = (stippleModel, CallAddStippleMode)
+    global g_shadows_list
+    model = self._StippleManager__stippleToAddDescs[vehID][0]
+    if model.attached:
+        callbackID = BigWorld.callback(0.0, partial(self._StippleManager__addStippleModel, vehID))
+        self._StippleManager__stippleToAddDescs[vehID] = (model, callbackID)
         return
     del self._StippleManager__stippleToAddDescs[vehID]
-    BigWorld.player().addModel(stippleModel)
+    BigWorld.player().addModel(model)
     vehicle = BigWorld.player().arena.vehicles.get(vehID)
-    if g_disappearDelayEnabled and BigWorld.player().team != vehicle['team']:
-        if g_disappearCaption:
-            vehicleType = unicode(vehicle['vehicleType'].type.shortUserString, 'utf-8')
-            TransBoundingBox = GUI.BoundingBox('objects/shadow/null.dds')
-            TransBoundingBox.size = (0.05, 0.05)
-            my_info = '\\cFF4949FF;' + vehicleType
-            TransBoundingBox.my_string = GUI.Text(my_info)
-            TransBoundingBox.my_string.colourFormatting = True
-            TransBoundingBox.my_string.colour = (255, 0, 0, 255)
-            TransBoundingBox.my_string.font = 'hpmp_panel.font'
-            TransBoundingBox.my_string.horizontalPositionMode = TransBoundingBox.my_string.verticalPositionMode = 'CLIP'
-            TransBoundingBox.my_string.widthMode = TransBoundingBox.my_string.heightMode = 'PIXEL'
-            TransBoundingBox.my_string.verticalAnchor = 'CENTER'
-            TransBoundingBox.my_string.horizontalAnchor = 'CENTER'
-            TransBoundingBox.source = stippleModel.bounds
-            TransBoundingBox.my_string.position = (0.5, 0.75, 0)
-            GUI.addRoot(TransBoundingBox)
-            g_times.append({'time': BigWorld.time(), 'bb': TransBoundingBox})
-            BigWorld.callback(g_disappearDelay, delBoundingBox)
-        CallAddStippleMode = BigWorld.callback(g_disappearDelay, partial(self._StippleManager__removeStippleModel, vehID))
+    if g_active and vehicle['isAlive'] and BigWorld.player().team != vehicle['team']:
+        vehicleType = unicode(vehicle['vehicleType'].type.shortUserString, 'utf-8')
+        TransBoundingBox = GUI.BoundingBox('objects/shadow/null.dds')
+        TransBoundingBox.size = (0.05, 0.05)
+        my_info = '\\cFF4949FF;' + vehicleType
+        TransBoundingBox.my_string = GUI.Text(my_info)
+        TransBoundingBox.my_string.colourFormatting = True
+        TransBoundingBox.my_string.colour = (255, 0, 0, 255)
+        TransBoundingBox.my_string.font = 'hpmp_panel.font'
+        TransBoundingBox.my_string.horizontalPositionMode = TransBoundingBox.my_string.verticalPositionMode = 'CLIP'
+        TransBoundingBox.my_string.widthMode = TransBoundingBox.my_string.heightMode = 'PIXEL'
+        TransBoundingBox.my_string.verticalAnchor = 'CENTER'
+        TransBoundingBox.my_string.horizontalAnchor = 'CENTER'
+        TransBoundingBox.source = model.bounds
+        TransBoundingBox.my_string.position = (0.5, 0.75, 0)
+        GUI.addRoot(TransBoundingBox)
+        LOG_DEBUG('add shadow: id=%d' % vehID)
+        g_shadows_list.append({'time': BigWorld.time(), 'bb': TransBoundingBox, 'id': vehID})
+        BigWorld.callback(g_delay, delBoundingBox)
+        callbackID = BigWorld.callback(g_delay, partial(self._StippleManager__removeStippleModel, vehID))
     else:
-        CallAddStippleMode = BigWorld.callback(_VEHICLE_DISAPPEAR_TIME, partial(self._StippleManager__removeStippleModel, vehID))
-    self._StippleManager__stippleDescs[vehID] = (stippleModel, CallAddStippleMode)
+        callbackID = BigWorld.callback(_VEHICLE_DISAPPEAR_TIME, partial(self._StippleManager__removeStippleModel, vehID))
+    self._StippleManager__stippleDescs[vehID] = (model, callbackID)
 
 def delBoundingBox():
-    global g_times
-    global g_disappearDelay
-    for value in g_times:
-        if BigWorld.time() - value['time'] >= g_disappearDelay:
+    global g_shadows_list
+    for value in g_shadows_list:
+        if BigWorld.time() - value['time'] >= g_delay:
+            LOG_DEBUG('timer: remove: id=%d' % value['id'])
             GUI.delRoot(value['bb'])
-            g_times.remove(value)
+            g_shadows_list.remove(value)
 
 VehicleAppearance = StippleManager._StippleManager__addStippleModel
-if g_disappearDelayEnabled:
-    StippleManager._StippleManager__addStippleModel = new_addStippleModel
+StippleManager._StippleManager__addStippleModel = new_addStippleModel
 
+'''
+from gui.Scaleform.Battle import Battle
+
+def onVehicleKilled(targetID, atackerID, *args):
+    global g_shadows_list
+    LOG_DEBUG('killed: id=%d' % targetID)
+    for value in g_shadows_list:
+        if value['id'] == targetID:
+            LOG_DEBUG('killed: remove: id=%d' % value['id'])
+            GUI.delRoot(value['bb'])
+            g_shadows_list.remove(value)
+            return
+
+def new_startBattle(current):
+    BigWorld.player().arena.onVehicleKilled += onVehicleKilled
+    old_startBattle(current)
+
+old_startBattle = Battle.afterCreate
+Battle.afterCreate = new_startBattle
+
+def new_stopBattle(current):
+    BigWorld.player().arena.onVehicleKilled -= onVehicleKilled
+    old_stopBattle(current)
+
+old_stopBattle = Battle.beforeDelete
+Battle.beforeDelete = new_stopBattle
+'''
