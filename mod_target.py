8b818328fdc5c5f3252a236bf30bbc38c08ff7b9
ushmal <ushmal@gmail.com>
2015-11-04 22:08:57 +0300

    add indicator

--- mod_target.py
+++ mod_target.py
@@ -7,18 +7,35 @@ from Account import PlayerAccount
 from gui.Scaleform.Battle import Battle
 from constants import ARENA_PERIOD
 from gui.app_loader import g_appLoader
+from tutorial.gui.Scaleform.battle.legacy import ScaleformLayout
+from tutorial.gui.Scaleform.battle.layout import BattleLayout
 from debug_utils import *
 
 g_visible_list = []
-g_edged_list = []
+g_target_list = []
+g_battle = False
+g_indicator = None
 
 def showBattleMsg(msg, color = 'red'):
     if g_appLoader.getDefBattleApp() is not None:
         g_appLoader.getDefBattleApp().call('battle.PlayerMessagesPanel.ShowMessage', ['0', msg, color])
 
+def addIndicator(id, color = 'red'):
+    global g_indicator
+    g_indicator = BattleLayout(ScaleformLayout)._getDirectionIndicator()
+    g_indicator.setShape(color) #'red' or 'green'
+    g_indicator.setDistance(vehicleDistance(id))
+    g_indicator.track(BigWorld.entity(id).position)
+
+def trackIndicator(id):
+    g_indicator.setDistance(vehicleDistance(id))
+    g_indicator.track(BigWorld.entity(id).position)
+
+def delIndicator():
+    g_indicator.remove()
+
 def addEdge(id):
     target = BigWorld.entity(id)
-    #FIXME read cfg and ...
     BigWorld.wgSetEdgeDetectColors((Math.Vector4(0.5, 0.5, 0.5, 1), Math.Vector4(1.0, 0.07, 0.027, 1), Math.Vector4(0.488, 0.839, 0.023, 1), Math.Vector4(0.9, 0.8, 0.1, 1)))
     BigWorld.wgAddEdgeDetectEntity(target, 3, 2, False)
 
@@ -28,15 +45,15 @@ def delEdge(id):
 
 def new_targetBlur(current, prevEntity):
     old_targetBlur(current, prevEntity)
-    if prevEntity.id in g_edged_list:
+    if prevEntity.id in g_target_list:
         BigWorld.wgAddEdgeDetectEntity(prevEntity, 3, 2, False)
 
 old_targetBlur = PlayerAvatar.targetBlur
 PlayerAvatar.targetBlur = new_targetBlur
 
 def new_targetFocus(current, entity):
-    if entity.id in g_edged_list:
-        BigWorld.wgDelEdgeDetectEntity(entity)
+    #if entity.id in g_target_list:
+    BigWorld.wgDelEdgeDetectEntity(entity)
     old_targetFocus(current, entity)
 
 old_targetFocus = PlayerAvatar.targetFocus
@@ -58,27 +75,32 @@ def isCollide(id):
 def checkCollides():
     for id in g_visible_list:
         if isCollide(id):
-            if id not in g_edged_list:
+            if id not in g_target_list:
                 addEdge(id)
-                g_edged_list.append(id)
+                g_target_list.append(id)
                 #veh = BigWorld.player().arena.vehicles.get(id)
                 #showBattleMsg('addEdge: %s (%s): %sm' % (veh['name'], veh['vehicleType'].type.shortUserString, int(vehicleDistance(id))))
         else:
-            if id in g_edged_list:
+            if id in g_target_list:
                 delEdge(id)
-                g_edged_list.remove(id)
+                g_target_list.remove(id)
                 #veh = BigWorld.player().arena.vehicles.get(id)
                 #showBattleMsg('delEdge: %s (%s): %sm' % (veh['name'], veh['vehicleType'].type.shortUserString, int(vehicleDistance(id))))
-    if hasattr(BigWorld.player(), 'arena'):
-        BigWorld.callback(0.2, lambda : checkCollides())
+    if g_battle:
+        BigWorld.callback(0.2, checkCollides)
 
 def new_onArenaPeriodChange(current, period, periodEndTime, periodLength, periodAdditionalInfo):
     old_onArenaPeriodChange(current, period, periodEndTime, periodLength, periodAdditionalInfo)
+    global g_visible_list, g_target_list, g_battle
     if period == ARENA_PERIOD.BATTLE:
-        global g_visible_list, g_edged_list
         g_visible_list = []
-        g_edged_list = []
-        BigWorld.callback(0.2, lambda : checkCollides())
+        g_target_list = []
+        g_battle = True
+        checkCollides()
+    if period == ARENA_PERIOD.AFTERBATTLE:
+        #g_visible_list = []
+        #g_target_list = []
+        g_battle = False
 
 old_onArenaPeriodChange = PlayerAvatar._PlayerAvatar__onArenaPeriodChange
 PlayerAvatar._PlayerAvatar__onArenaPeriodChange = new_onArenaPeriodChange
@@ -95,13 +117,13 @@ old_vehicle_onEnterWorld = PlayerAvatar.vehicle_onEnterWorld
 PlayerAvatar.vehicle_onEnterWorld = new_vehicle_onEnterWorld
 
 def new_vehicle_onLeaveWorld(current, vehicle):
-    old_vehicle_onLeaveWorld(current, vehicle)
     id = vehicle.id
     if id in g_visible_list:
         g_visible_list.remove(id)
-    if id in g_edged_list:
+    if id in g_target_list:
         delEdge(id)
-        g_edged_list.remove(id)
+        g_target_list.remove(id)
+    old_vehicle_onLeaveWorld(current, vehicle)
 
 old_vehicle_onLeaveWorld = PlayerAvatar.vehicle_onLeaveWorld
 PlayerAvatar.vehicle_onLeaveWorld = new_vehicle_onLeaveWorld
@@ -110,9 +132,9 @@ def onVehicleKilled(targetID, atackerID, *args):
     id = targetID
     if id in g_visible_list:
         g_visible_list.remove(id)
-    if id in g_edged_list:
+    if id in g_target_list:
         delEdge(id)
-        g_edged_list.remove(id)
+        g_target_list.remove(id)
 
 def new_startBattle(current):
     BigWorld.player().arena.onVehicleKilled += onVehicleKilled
