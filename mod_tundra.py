8b16d9286927c9111654dc4f9eeb9225b2722352
ushmal <ushmal@gmail.com>
2015-11-04 22:08:19 +0300

    fix alt

--- mod_tundra.py
+++ mod_tundra.py
@@ -4,19 +4,15 @@ from Avatar import PlayerAvatar
 from AvatarInputHandler import AvatarInputHandler
 from debug_utils import *
 
-g_tundra = False
-
 def new_onControlModeChanged(current, eMode, **args):
     global g_tundra
     old_onControlModeChanged(current, eMode, **args)
     if eMode == 'sniper':
         BigWorld.wg_setTreeHidingRadius(750, 0)
         BigWorld.wg_enableTreeTransparency(False)
-        g_tundra = True
     else:
         BigWorld.wg_enableTreeHiding(False)
         BigWorld.wg_enableTreeTransparency(True)
-        g_tundra = False
 
 old_onControlModeChanged = AvatarInputHandler.onControlModeChanged
 AvatarInputHandler.onControlModeChanged = new_onControlModeChanged
@@ -26,12 +22,12 @@ def new_PlayerHandleKey(current, isDown, key, mods):
         if key == Keys.KEY_LALT:
             player = BigWorld.player()
             if player.inputHandler.ctrl == player.inputHandler.ctrls['sniper']:
-                if (isDown and g_tundra) or (not isDown and not g_tundra):
-                    BigWorld.wg_setTreeHidingRadius(750, 0)
-                    BigWorld.wg_enableTreeTransparency(False)
-                else:
+                if isDown:
                     BigWorld.wg_setTreeHidingRadius(12, 15)
                     BigWorld.wg_enableTreeTransparency(True)
+                else:
+                    BigWorld.wg_setTreeHidingRadius(750, 0)
+                    BigWorld.wg_enableTreeTransparency(False)
             else:
                 if isDown:
                     BigWorld.wg_enableTreeHiding(True)
