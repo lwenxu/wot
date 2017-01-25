8f478ea12692e489f3d4cc6fcdeae63565623776
ushmal <ushmal@gmail.com>
2017-01-25 22:20:48 +0300

    update fast_repair

--- mod_fast_repair.py
+++ mod_fast_repair.py
@@ -1,46 +1,65 @@
 import BigWorld, ResMgr, Keys, random
-from functools import partial
 from Avatar import PlayerAvatar
 from gui.Scaleform.daapi.view.meta.DamagePanelMeta import DamagePanelMeta
-from skeletons.gui.battle_session import IBattleSessionProvider
-from helpers import dependency
+from gui.Scaleform.daapi.view.battle.shared.consumables_panel import ConsumablesPanel
 from constants import ARENA_PERIOD
 from debug_utils import *
 
-g_trackRepairKey = Keys.KEY_SPACE
+g_fastKey = Keys.KEY_SPACE
 g_repairKey = Keys.KEY_4
 g_healKey = Keys.KEY_5
-g_dev_state = {}
-g_guiSessionProvider = dependency.descriptor(IBattleSessionProvider)
-g_repair_list = ['engine', 'ammoBay', 'gun', 'turretRotator', 'surveyingDevice', 'radio', 'rightTrack', 'leftTrack']
-g_heal_list = ['commander', 'gunner1', 'gunner2', 'driver', 'loader1', 'loader2']
-g_repair_critical =  ['engine', 'ammoBay', 'gun', 'turretRotator']
-g_repair_destroyed = ['engine', 'gun', 'turretRotator', 'surveyingDevice']
+g_fired = False
+g_damaged = set([])
+g_destroyed = set([])
+g_repair_list = set(['engine', 'ammoBay', 'gun', 'turretRotator', 'surveyingDevice', 'radio', 'rightTrack', 'leftTrack'])
+g_heal_list = set(['commander', 'gunner1', 'gunner2', 'driver', 'loader1', 'loader2'])
+g_repair_fast = set(['rightTrack', 'leftTrack', 'chassis'])
+g_repair_damaged =  set(['engine', 'ammoBay', 'gun', 'turretRotator'])
+g_repair_destroyed = set(['engine', 'gun', 'turretRotator', 'surveyingDevice'])
+g_ConsumablesPanel = None
 
 LOG_DEBUG = LOG_NOTE
 
-def repair(tag, entityName, timeout = 0.0):
-    ctrl = g_guiSessionProvider.shared.equipments
-    if ctrl is None:
-        LOG_DEBUG('no equipments to repair')
-        return
-    BigWorld.callback(timeout, partial(ctrl.changeSettingByTag, tag, entityName, BigWorld.player()))
-    LOG_DEBUG('apply %s to %s' % (tag, entityName))
+def repair(device):
+    LOG_DEBUG('repairing %s' % device)
+    g_ConsumablesPanel._ConsumablesPanel__handleEquipmentPressed(1275, device) #repairkit
+    g_damaged.remove(device)
+    g_destroyed.remove(device)
 
-'''
+def heal(device):
+    LOG_DEBUG('healing %s' % device)
+    g_ConsumablesPanel._ConsumablesPanel__handleEquipmentPressed(763, device) #medkit
+    g_damaged.remove(device)
+    g_destroyed.remove(device)
+
+def extinguish():
+    g_ConsumablesPanel._ConsumablesPanel__handleEquipmentPressed(251) #extinguisher
+    global g_fired
+    g_fired = False
+    LOG_DEBUG('fire extinguished')
+
+def new_onEquipmentAdded(self, int_cd, item):
+    old_onEquipmentAdded(self, int_cd, item)
+    global g_ConsumablesPanel
+    g_ConsumablesPanel = self
+    LOG_DEBUG('added: %s: %s' % (int_cd, item))
+
+old_onEquipmentAdded = ConsumablesPanel._ConsumablesPanel__onEquipmentAdded
+ConsumablesPanel._ConsumablesPanel__onEquipmentAdded = new_onEquipmentAdded
+
 def new_as_setFireInVehicleS(self, isInFire):
     old_as_setFireInVehicleS(self, isInFire)
-    repair('extinguisher', None, 1 + random.random());
+    global g_fired
+    g_fired = True
 
 old_as_setFireInVehicleS = DamagePanelMeta.as_setFireInVehicleS
 DamagePanelMeta.as_setFireInVehicleS = new_as_setFireInVehicleS
-'''
 
 def new_as_updateDeviceStateS(self, deviceName, deviceState):
-    global g_dev_state
     LOG_DEBUG('%s ==> %s:' % (deviceName, deviceState))
-    g_dev_state[deviceName] = deviceState
-    old_as_updateDeviceStateS(self, deviceName, deviceState) #FIXME Exception: PyGFxValue - Failed to invoke method as_updateDeviceState
+    if deviceState == 'critical': g_damaged.add(deviceName)
+    if deviceState == 'destroyed': g_destroyed.add(deviceName)
+    old_as_updateDeviceStateS(self, deviceName, deviceState)
 
 old_as_updateDeviceStateS = DamagePanelMeta.as_updateDeviceStateS
 DamagePanelMeta.as_updateDeviceStateS = new_as_updateDeviceStateS
@@ -48,45 +67,30 @@ DamagePanelMeta.as_updateDeviceStateS = new_as_updateDeviceStateS
 def new_handleKey(self, isDown, key, mods):
     player = BigWorld.player()
     if player and player.isOnArena and player.inputHandler:
-
-        # fast track repair
-        if key == g_trackRepairKey:
-            for deviceName in ['rightTrack', 'leftTrack', 'engine']:
-                if g_dev_state.has_key(deviceName):
-                    if g_dev_state[deviceName] == 'destroyed':
-                        repair('repairkit', deviceName)
-                        return True
-
+        # fast action
+        if key == g_fastKey:
+            if g_fired:
+                extinguish()
+                return True
+            devices = g_repair_fast & g_repair_destroyed
+            LOG_DEBUG('devices to repair: %s' % devices)
+            if len(devices) == 1:
+                repair(devices.pop())
+                return True
         # fast repair
         if key == g_repairKey:
-            LOG_DEBUG('g_dev_state: ' % g_dev_state)
-            deviceToRepair = ''
-            # find device to repair
-            for deviceName,deviceState in g_dev_state.iteritems():
-                if ((deviceState == 'critical' and deviceName in g_repair_critical) or
-                    (deviceState == 'destroyed' and deviceName in g_repair_destroyed)):
-                    if deviceToRepair:
-                        deviceToRepair = ''
-                        break
-                    deviceToRepair = deviceName
-            if deviceToRepair:
-                repair('repairkit', deviceToRepair)
+            devices = (g_damaged & g_repair_damaged) | (g_destroyed & g_repair_destroyed)
+            LOG_DEBUG('devices to repair: %s' % devices)
+            if len(devices) == 1:
+                repair(devices.pop())
                 return True
-
         # fast heal
         if key == g_healKey:
-            deviceToRepair = ''
-            # find crew member to repair
-            for deviceName,deviceState in g_dev_state.iteritems():
-                if deviceState == 'destroyed' and deviceName in g_heal_list:
-                    if deviceToRepair:
-                        deviceToRepair = ''
-                        break
-                    deviceToRepair = deviceName
-            if deviceToRepair:
-                repair('medkit', deviceToRepair)
+            devices = g_destroyed & g_heal_list
+            LOG_DEBUG('devices to repair: %s' % devices)
+            if len(devices) == 1:
+                heal(devices.pop())
                 return True
-
     return old_handleKey(self, isDown, key, mods)
 
 old_handleKey = PlayerAvatar.handleKey
@@ -94,13 +98,11 @@ PlayerAvatar.handleKey = new_handleKey
 
 def new_onArenaPeriodChange(current, period, periodEndTime, periodLength, periodAdditionalInfo):
     old_onArenaPeriodChange(current, period, periodEndTime, periodLength, periodAdditionalInfo)
-    global g_dev_state
-    if period == ARENA_PERIOD.BATTLE:
-        LOG_DEBUG('ARENA_PERIOD.BATTLE')
-        g_dev_state = {}
-    elif period == ARENA_PERIOD.AFTERBATTLE:
-        LOG_DEBUG('ARENA_PERIOD.AFTERBATTLE')
-        g_dev_state = {}
+    global g_damaged, g_destroyed
+    if period == ARENA_PERIOD.BATTLE or period == ARENA_PERIOD.AFTERBATTLE:
+        LOG_DEBUG('clearing lists')
+        g_damaged.clear()
+        g_destroyed.clear()
 
 old_onArenaPeriodChange = PlayerAvatar._PlayerAvatar__onArenaPeriodChange
 PlayerAvatar._PlayerAvatar__onArenaPeriodChange = new_onArenaPeriodChange
